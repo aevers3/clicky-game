258f3d3ec3e20a660ce225000e4c1dafbfc0fb4e
aevers3 <aevers3@users.noreply.github.com>
2019-06-18 14:06:28 -0400

    navbar alignment fix

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -7,13 +7,19 @@ import './style.css';
 function Navbar() {
     return (
         <div className='navbar fixed-top navbar-custom p-3 d-flex justify-content-around'>
-            <HomeLink 
-                text='Clicky Game'
-            />
-            <WelcomeMessage
-                text='Click an image to begin!'
-            />
-            <Scoreboard />
+            <div className='col-4 text-center'>
+                <HomeLink
+                    text='Clicky Game'
+                />
+            </div>
+            <div className='col-4 text-center'>
+                <WelcomeMessage
+                    text='Click an image to begin!'
+                />
+            </div>
+            <div className='col-4 text-center'>
+                <Scoreboard />
+            </div>
         </div>
     )
 }
