b9aae0513c3ccd4e0af2d41ae8c94e9a617a14e5
aevers3 <aevers3@users.noreply.github.com>
2019-06-21 10:49:36 -0400

    Updates

---
+++ precache-manifest.e6b2db8b0fac37d829097fc4d8038909.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e9c8782a559ddc7b331c6a32adeb3a3",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "9064e48824ad294b7083",
+    "url": "/clicky-game/static/css/main.bb691911.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "9064e48824ad294b7083",
+    "url": "/clicky-game/static/js/main.0a0e400e.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
